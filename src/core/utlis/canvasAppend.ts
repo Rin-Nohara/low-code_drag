42c5ab37d32126c0691f857da02c975254ad2d29
Galen <61968242+gaearon-byte@users.noreply.github.com>
2022-03-06 23:25:11 +0800

    fix: build fail

--- src/core/utlis/canvasAppend.ts
+++ src/core/utlis/canvasAppend.ts
@@ -3,6 +3,7 @@ import * as ReactDOM from "react-dom";
 import {componentJsonType} from './typings'
 
 export interface IProps {
+    // todo componentJson 应为 required
     componentJson?: componentJsonType;
     canvasContainer?: HTMLElement | null | undefined;
     position: {
@@ -19,6 +20,7 @@ let cacheRoot: NullOrElement = null;
 const jsonToDom = (props: IProps): HTMLElement => {
 
     const { componentJson, canvasContainer = null, position } = props;
+    console.log('componentJson == ', componentJson)
     let rootDom: NullOrElement = null;
     if(!canvasContainer) {
         rootDom = cacheRoot;
@@ -31,11 +33,18 @@ const jsonToDom = (props: IProps): HTMLElement => {
     }
     cacheRoot = canvasContainer
 
-    // todo componentJson 转为 createElement 可识别的格式
+    /*
+        todo componentJson 转为 createElement 可识别的格式
+        现在为 写死的 h1 标签，后续 TODO:
+            1. 解析 jsonSchema 将对应组件渲染
+            2. 添加 componentId 和 renderId，优化性能
+            3. 支持 custom 组件
+     */
     // @ts-ignore
     return ReactDOM.render(createElement(
         'h1',
         {
+            // 渲染时必须的三个样式
             style: {
                 position: 'absolute',
                 top: position.y,
